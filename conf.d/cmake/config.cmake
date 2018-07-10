63dc64fb90311eb1942d81e32e1a879798403258
Romain Forlot <romain.forlot@iot.bzh>
2018-07-10 19:03:22 +0200

    Bump Submodules
    Submodule afb-helpers 5811a53..52f0478:
      > Remove unnecessary 'AFB_DEBUG' redefinition
      > Use binding version to handle dynapi
    
    Submodule app-controller-submodule e45d063..440be09:
      > Use binding version to set controller definitions
      > ctl-lua: typo fix
      > asynchronism for test: LockWait added
      > Fix: plugin api assignement order
    
    Submodule conf.d/app-templates 0880356..332f377:
      > Be able to overwrite BUILD_TYPE using CLI
      > Use CACHE variable for other common CMAKE variable
    
    Change-Id: I455c7132ccd9266b8e77a5a8c02fd7501f577c11
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- conf.d/cmake/config.cmake
+++ conf.d/cmake/config.cmake
@@ -77,7 +77,7 @@ set (PKG_REQUIRED_LIST
 # Prefix path where will be installed the files
 # Default: /usr/local (need root permission to write in)
 # ------------------------------------------------------
-set(CMAKE_INSTALL_PREFIX $ENV{HOME}/opt)
+set(INSTALL_PREFIX $ENV{HOME}/opt)
 
 # Customize link option
 # -----------------------------
@@ -129,7 +129,7 @@ set(CONTROL_SUPPORT_LUA 1)
 add_definitions(-DCONTROL_PLUGIN_PATH="${CMAKE_BINARY_DIR}/package/lib/plugins:${CMAKE_BINARY_DIR}/package/var:${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}/lib/plugins")
 add_definitions(-DCONTROL_CONFIG_PATH="${CMAKE_BINARY_DIR}/package/etc:${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}/etc")
 add_definitions(-DCTL_PLUGIN_MAGIC=1286576532)
-add_definitions(-DUSE_API_DYN=1)
+add_definitions(-DUSE_API_DYN=1 -DAFB_BINDING_VERSION=3 -DAFB_BINDING_WANT_DYNAPI)
 
 # (BUG!!!) as PKG_CONFIG_PATH does not work [should be an env variable]
 # ---------------------------------------------------------------------
